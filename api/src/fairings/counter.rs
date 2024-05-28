b6a2ec15c8703b1e1a5f364f8c1117e3cef988cc
ibrahizy <imahmood1710@gmail.com>
2024-05-28 01:20:19 +0100

    initial implimentation for updating counter based on interval

--- api/src/fairings/counter.rs
+++ api/src/fairings/counter.rs
@@ -1,6 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::sync::Arc;
 use std::sync::Mutex;
+
+use std::thread;
+use std::time::Duration;
 
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Status;
@@ -16,7 +20,11 @@ pub struct Endpoint {
 
 #[derive(Default, Debug)]
 pub struct Counter {
-    pub stats: Mutex<HashMap<String, Endpoint>>,
+    pub stats: Arc<Mutex<HashMap<String, Endpoint>>>,
+}
+
+impl Counter {
+    const UPDATE_INTERVAL: Duration = Duration::from_secs(600);
 }
 
 #[rocket::async_trait]
@@ -28,33 +36,36 @@ impl Fairing for Counter {
         }
     }
 
-    async fn on_request(&self, req: &mut Request<'_>, _: &mut Data<'_>) {
-        match self.stats.lock() {
-            Ok(mut stats) => {
-                let endpoint = stats.entry(req.uri().to_string()).or_insert(Endpoint {
-                    requests: 0,
-                    successful_responses: 0,
-                });
-                endpoint.requests += 1;
-            }
-            Err(poisoned) => {
-                eprintln!("Poisoned lock: {:?}", poisoned);
-            }
+	async fn on_liftoff(&self, _: &rocket::Rocket<rocket::Orbit>) {
+    let stats = self.stats.clone();
+                                                                
+    thread::spawn(move || {
+        loop {
+            thread::sleep(Self::UPDATE_INTERVAL);
+                                                                
+            let stats = stats.lock().expect("poisoned lock");
+            db::counter::update_counter(&stats)
         }
+    });
+}
+
+    async fn on_request(&self, req: &mut Request<'_>, _: &mut Data<'_>) {
+        let mut stats = self.stats.lock().expect("poisoned lock");
+
+        let endpoint = stats.entry(req.uri().to_string()).or_insert(Endpoint {
+            requests: 0,
+            successful_responses: 0,
+        });
+        endpoint.requests += 1;
     }
 
     async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
         let status = res.status();
         if status == Status::Ok {
-            match self.stats.lock() {
-                Ok(mut stats) => {
-                    if let Some(endpoint) = stats.get_mut(&req.uri().to_string()) {
-                        endpoint.successful_responses += 1;
-                    }
-                }
-                Err(poisoned) => {
-                    eprintln!("Poisoned lock: {:?}", poisoned);
-                }
+            let mut stats = self.stats.lock().expect("poisoned lock");
+
+            if let Some(endpoint) = stats.get_mut(&req.uri().to_string()) {
+                endpoint.successful_responses += 1;
             }
         }
     }
